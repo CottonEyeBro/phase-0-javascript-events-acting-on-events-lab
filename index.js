fb9c2b26bfbc9fc1d6e8804c9245b25ba38799a1
Cooper <55swimmer55@gmail.com>
2023-08-14 14:56:06 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+
+// Move Dodger Left
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+
+// Move Dodger Right
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
